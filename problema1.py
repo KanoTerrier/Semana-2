e8b8447a11d12ed4510f5692d9b25363cbee0e35
KanoTerrier <kanoterrier@gmail.com>
2026-04-24 15:32:35 -0500

    no se subeeeeee

--- problema1.py
+++ problema1.py
@@ -2,3 +2,17 @@
 Pida el nombre del cleinte, cuantas manzanas comprará y un precio. 
 Muestre el nombre y el total a pagar
 """
+print("Ingrese su nombre: ")
+nomCli = input()
+
+print(f"Hola {nomCli}")
+
+print("¿Cuántas manzanas deseas comprar? ")
+Comp = int(input())
+
+print(f"Deseo comprar {Comp}")
+
+manzana = 2
+costo_final = manzana * Comp
+
+print(f"Tienes que pagar {costo_final}")
